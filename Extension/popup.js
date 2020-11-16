250221009af949b44987414b0aad342969e08e28
Saurav <Saurav.KC14@myhunter.cuny.edu>
2020-11-15 20:16:19 -0500

    Updated images and added method to obtain active tab's url

--- Extension/popup.js
+++ Extension/popup.js
@@ -9,3 +9,13 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+
+chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true
+}, function(tabs) {
+  // and use that tab to fill in out title and url
+  var tab = tabs[0];
+  // console.log(tab.url);
+  // alert(tab.url);
+});
