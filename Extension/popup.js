f8f631d43e13b1e9304a47ae786f3f42ee23e9b3
Prerna Agarwal <prerna2896@gmail.com>
2020-11-16 18:43:37 -0500

    Add code for text scraper

--- Extension/popup.js
+++ Extension/popup.js
@@ -19,3 +19,40 @@ chrome.tabs.query({
   // console.log(tab.url);
   // alert(tab.url);
 });
+
+// extract privacy URL from the document
+var urls = [];
+
+// the privacy URL of the website
+var privacyURL;
+
+for(var i = document.links.length; i --> 0;)
+	var name = document.links[i].href;
+	if (name.indexOf("privacy-policy") > -1) {
+		privacyURL = name;
+	} else if (name.indexOf("privacy") > -1) {
+		privacyURL = name;
+	}
+	// saving all the URLs for future work
+  urls.push(name);
+  
+// extract text from privacy URL
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// variable that stores text to be used for analytics
+var data;
+
+axios.get(url)
+  .then(response => {
+    const html = response.data;
+	const $ = cheerio.load(html);
+	// console.log(html);
+	$('div').each(function(i, elem) {
+		data = $(this).text().trim();
+		console.log($(this).find('p').text().trim());
+	});
+  })
+  .catch(error => {
+    console.log(error);
+  })
