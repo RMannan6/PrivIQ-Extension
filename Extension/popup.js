805df8821abdd4e58ff5b08d3f4063c018261ce5
Joseph D'Agostino <Joseph.D'Agostino@stockton.edu>
2020-11-20 14:23:22 -0500

    added MIT license to the output

--- Extension/popup.js
+++ Extension/popup.js
@@ -65,6 +65,8 @@ const privacyTerms = [
 ];
 const objectOutput = document.getElementById("returnedObject");
 
+const license = "<p>Copyright 2020 PRIVIQ</p><p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p><p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p><p>THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>";
+
 // we need to find a way to get the privacy info to this testValue variable!!!
 //
 let testValue = "Scrapped privacy text would be set to this variable!";
@@ -120,9 +122,9 @@ function evaluateKeyPhrases(data) {
   });
   if (privacyTermMatches > 3) {
     objectOutput.innerHTML +=
-      "<h1 class='match-response true'>This site will collect your data</h1>";
+      "<h1 class='match-response true'>This site will collect your data</h1>" + license;
   } else {
     objectOutput.innerHTML +=
-      "<h1 class='match-response false'>This site may collect your data</h1>";
+      "<h1 class='match-response false'>This site may collect your data</h1>" + license;
   }
 }
